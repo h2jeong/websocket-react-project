83dba705f69bbda8dc60de498d748c455d337dbe
ZOE <theonlyoneu@daum.net>
2021-03-08 12:20:38 +0900

    upload

---
+++ src/components/views/mapPage/GeoMap.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { drawXYs } from '../../../../plugins/map/draw';
+import { setZoom } from '../../../../plugins/map/event';
+import { olInit } from '../../../../plugins/map/meta';
+
+const GeoMap = ({ width, height, project, mark }) => {
+  useEffect(() => {
+    if (!project) return;
+
+    const { geoserver, workspace, layers } = project;
+
+    olInit(geoserver, workspace, layers);
+
+    if (mark) drawXYs(mark, 'local');
+
+    setZoom(false);
+  }, [project]);
+
+  return (
+    <>
+      <div id="ol" style={{ width, height }} />
+      <div id="naver" style={{ width, height }} />
+    </>
+  );
+};
+
+export default GeoMap;

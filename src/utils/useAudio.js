b1bbd5039c7dce06e02871059ebd76e03c799400
ZOE <theonlyoneu@daum.net>
2021-03-08 12:26:24 +0900

    upload

---
+++ src/utils/useAudio.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+const useAudio = () => {
+  const [audio] = useState(
+    // new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3')
+    new Audio()
+  );
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
+  const toggle = () => setPlaying(!playing);
+
+  return [playing, toggle];
+};
+
+export default useAudio;
---
+++ src/utils/useKeyboard.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+const useKeyboard = (targetKey, cb) => {
+  const [keyPressed, setKeyPressed] = useState(false);
+
+  const downHandler = ({ key }) => {
+    console.log('down:', key, targetKey);
+    if (targetKey === key) {
+      setKeyPressed(true);
+      cb();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', downHandler);
+
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+    };
+  }, []);
+
+  return keyPressed;
+};
+
+export default useKeyboard;

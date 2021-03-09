739fe1be3f9fde004b9a70887d0214f330d18ab8
ZOE <theonlyoneu@daum.net>
2021-03-09 18:34:22 +0900

    update

--- src/components/views/common/SensorSwitch.jsx
+++ src/components/views/common/SensorSwitch.jsx
@@ -13,10 +13,10 @@ const SensorSwitch = ({ value, className }) => {
 
     for (let i = 0; i < names.length; i += 1) {
       const name = names[i];
-      if (Array.isArray(curr[name])) {
-        acc[name].push(...curr[name]);
-      }
+
+      if (Array.isArray(curr[name])) acc[name].push(...curr[name]);
     }
+
     return acc;
   }, allStatus);
 
